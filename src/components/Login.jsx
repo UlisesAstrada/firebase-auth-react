12dbd9def73146d81f6b7e9595c73820ce51e600
Ulises <ulises_astrada@outlook.com>
2021-02-26 23:56:17 -0300

    Login component added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 function Login() {
   return (
-    <div className="row">
+    <div className="row mt-5">
       <div className="col"></div>
-      <div className="col"></div>
+      <div className="col">
+        <form className="form-group">
+          <input
+            className="form-control mb-3"
+            placeholder="Introduce tu email" 
+            type="text"/>
+          <input
+            className="form-control"
+            placeholder="Introduce tu contraseña" 
+            type="text"/>
+          <input 
+            className="btn btn-block mt-3 bg-success text-white rounded"
+            type="submit" 
+            value="Registrar usuario"/>
+        </form>
+      </div>
       <div className="col"></div>
     </div>
   )
